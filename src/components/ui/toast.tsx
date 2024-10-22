acf2aba7ce2db99751b79dae24dd61d46f6c0da0
Luis Pinto <lpinto1@ucol.mx>
2024-10-22 15:18:13 -0600

    chore: set toast styles

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -31,7 +31,7 @@ const toastVariants = cva(
       variant: {
         default: "border bg-white text-foreground",
         destructive:
-          "destructive group border-destructive bg-destructive text-destructive-foreground",
+          "destructive group bg-red-500 text-white",
       },
     },
     defaultVariants: {
