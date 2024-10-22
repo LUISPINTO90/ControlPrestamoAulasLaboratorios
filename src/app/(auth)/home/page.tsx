0b1fba67b18bc3da5ea5971aa25e2b757e344af1
Luis Pinto <lpinto1@ucol.mx>
2024-10-22 13:58:27 -0600

    chore: set home page

--- src/app/(auth)/home/page.tsx
+++ src/app/(auth)/home/page.tsx
@@ -1,9 +1,3 @@
 export default async function Home() {
-  return (
-    <div>
-      <h1 className="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none">
-        Reserva de espacios
-      </h1>
-    </div>
-  );
+  return <div className="space-y-6">Descripcion breve app</div>;
 }
