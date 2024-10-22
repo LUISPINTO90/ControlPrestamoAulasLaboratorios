2e7925ca3b7a30c1abdb57694ebc5dae514ad77f
Luis Pinto <lpinto1@ucol.mx>
2024-10-22 15:14:08 -0600

    chore: set home page

--- src/app/(auth)/home/page.tsx
+++ src/app/(auth)/home/page.tsx
@@ -1,3 +1,21 @@
+import { Card } from "@/components/ui/card";
+
 export default async function Home() {
-  return <div className="space-y-6">Descripcion breve app</div>;
+  return (
+    <div className="space-y-6">
+      <Card className="p-4">
+        <h1 className="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none">
+          Empieza a reservar espacios dentro de la{" "}
+          <span className="text-blue-600">Facultad de Telemática</span> ahora
+          mismo!
+        </h1>
+
+        <p className="text-gray-500 max-w-lg">
+          Bienvenido a PrestaSalones™, la plataforma de reservaciones de
+          espacios de la Facultad de Telemática. Aquí podrás reservar salones y
+          laboratorios para tus eventos, clases, reuniones y más.
+        </p>
+      </Card>
+    </div>
+  );
 }
